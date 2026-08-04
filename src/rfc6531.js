66c012582e515eb775facd401777b25443614ef7
Jason Desrosiers <jdesrosi@gmail.com>
2026-08-04 11:37:38 -0700

    Add UTF8-Non-Ascii for idn-email

--- src/rfc6531.js
+++ src/rfc6531.js
@@ -4,18 +4,20 @@ import { isIdn } from "./uts46.js";
  * @import * as API from "./index.d.ts"
  */
 
+const utf8NonAscii = `[\\u{0080}-\\u{D7FF}\\u{E000}-\\u{10FFFF}]`;
+
 const ucschar = `[\\u{A0}-\\u{D7FF}\\u{F900}-\\u{FDCF}\\u{FDF0}-\\u{FFEF}\\u{10000}-\\u{1FFFD}\\u{20000}-\\u{2FFFD}\\u{30000}-\\u{3FFFD}\\u{40000}-\\u{4FFFD}\\u{50000}-\\u{5FFFD}\\u{60000}-\\u{6FFFD}\\u{70000}-\\u{7FFFD}\\u{80000}-\\u{8FFFD}\\u{90000}-\\u{9FFFD}\\u{A0000}-\\u{AFFFD}\\u{B0000}-\\u{BFFFD}\\u{C0000}-\\u{CFFFD}\\u{D0000}-\\u{DFFFD}\\u{E1000}-\\u{EFFFD}]`;
 
 const alpha = `[a-zA-Z]`;
 const hexdig = `[\\da-fA-F]`;
 
 // Printable US-ASCII characters not including specials.
-const atext = `(?:[\\w!#$%&'*+\\-/=?^\`{|}~]|${ucschar})`;
+const atext = `(?:[\\w!#$%&'*+\\-/=?^\`{|}~]|${ucschar}|${utf8NonAscii})`;
 const atom = `${atext}+`;
 const dotString = `${atom}(?:\\.${atom})*`;
 
 // Any ASCII graphic or space without blackslash-quoting except double-quote and the backslash itself.
-const qtextSMTP = `(?:[\\x20-\\x21\\x23-\\x5B\\x5D-\\x7E]|${ucschar})`;
+const qtextSMTP = `(?:[\\x20-\\x21\\x23-\\x5B\\x5D-\\x7E]|${ucschar}|${utf8NonAscii})`;
 // backslash followed by any ASCII graphic or space
 const quotedPairSMTP = `\\\\[\\x20-\\x7E]`;
 const qcontentSMTP = `(?:${qtextSMTP}|${quotedPairSMTP})`;
@@ -43,7 +45,7 @@ const generalAddressLiteral = `${ldhStr}:${dcontent}+`;
 
 const addressLiteral = `\\[(?:${ipv4Address}|${ipv6AddressLiteral}|${generalAddressLiteral})\\]`;
 
-const mailbox = `(?<localPart>${localPart})@(?:(?<ip>${addressLiteral})|(?<domain>${domain}))`;
+const mailbox = `${localPart}@(?:${addressLiteral}|(?<domain>${domain}))`;
 
 const mailboxPattern = new RegExp(`^${mailbox}$`, "u");
 
